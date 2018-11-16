5e5b3d0431526de41a243154e33de60611657ef5
barbicorro <barbicorro@hotmail.com>
2018-11-15 23:56:11 -0300

    Se crea la subasta

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-<<<<<<< HEAD
+
 # Examples:
 #
 #locations = Location.create([{ country: 'Argentina', province: 'Buenos Aires', locality: 'La Plata' },
 
 #{ country: 'Argentina', province: 'Buenos Aires', locality: 'Berazategui' }])
 
-# Residence.create(title: 'Hotel 3', location: movies.first)
-location1= Location.create({country: 'Argentina', province: 'San Luis', locality:'Villa Mercedes'})
-location2= Location.create({country: 'Argentina', province: 'Neuquen', locality:'Villa La Angostura'})
-location3= Location.create({country: 'Argentina', province: 'Tierra del Fuego', locality:'Ushuaia'})
-location4= Location.create({country: 'Argentina', province: 'Mendoza', locality:'San Martin de los Andes'})
 
-#residences = Residence.create([{ title: 'Holtel 1',  }, { title: 'Hotel 2' }])
-resi1= Residence.create({location_id:"1", title: 'Hotel 1', address: 'calle 150 n1992',description:'hotel 5 estrellas'})
-resi2= Residence.create({location_id:"2", title: 'Hotel 2', address: 'calle 14 n899',description:'hotel con pileta'})
-resi3= Residence.create({location_id:"3", title: 'Hotel 3', address: 'calle 156 n192',description:'hotel con vista a las montañas'})
-resi4= Residence.create({location_id:"4", title: 'Hotel 4', address: 'calle 155 n203',description:'hotel con vista a las sierras'})
+
+
 
 #Week.create(status:string, residence_id:"")
 week1= Week.create({status: 'reservada', residence_id: "1"})
@@ -31,7 +23,6 @@ sale1=Sale.create({fechaInicio:"2019/05/03 19:00", precioBase: "5000",fechaFin:"
 sale2=Sale.create({fechaInicio:"2019/08/23 15:00", precioBase: "5000",fechaFin:"2019/08/26 15:00", week_id: "3" })
 sale1=Sale.create({fechaInicio:"2019/11/17 17:00", precioBase: "7000",fechaFin:"2019/11/20 17:00", week_id: "1" })
 sale1=Sale.create({fechaInicio:"2019/12/15 12:00", precioBase: "5000",fechaFin:"2019/12/18 12:00", week_id: "4" })
-=======
 
 
 resi1= Residence.create({title: 'Hotel 1', address: 'calle 150 n1992',description:'hotel 5 estrellas',
@@ -41,4 +32,4 @@ resi2= Residence.create({title: 'Hotel 2', address: 'calle 14 n899',description:
 resi3= Residence.create({title: 'Hotel 3', address: 'calle 156 n192', 
 	description:'hotel con vista a las montañas',country:'Argentina', province:'San Luis', locality:'Villa Mercedes',image: 'https://www.hotelsofgreece.com/athens/electra-palace-hotel-pool.jpg'})
 
->>>>>>> 088faa4ec070901d9bc2d3ce9c81ae27dc5470a8
+
