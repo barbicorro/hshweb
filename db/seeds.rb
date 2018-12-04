000457b62ce56f88f5d8199d75e84fb650c2b6a9
Pauli <paula.g.17@hotmail.com>
2018-12-03 21:15:44 -0300

    seed sin residencias

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@
 #
 
 
-resi1= Residence.create({title: 'Hotel 1', address: 'calle 150 n1992',description:'hotel 5 estrellas',
-	country:'Argentina', province:'Neuquen', locality:'Villa La Angostura', image: ' https://i.pinimg.com/originals/d1/24/11/d124114c0f8b03734018bf1716a0db3b.jpg'})
-resi2= Residence.create({title: 'Hotel 2', address: 'calle 14 n899',description:'hotel con pileta',
-	country:'Argentina', province:'Tierra del Fuego', locality:'Usuahia', image:' https://q-ec.bstatic.com/images/hotel/max1024x768/468/46871800.jpg'})
-resi3= Residence.create({title: 'Hotel 3', address: 'calle 156 n192',
-	description:'hotel con vista a las montañas',country:'Argentina', province:'San Luis', locality:'Villa Mercedes',image: 'https://www.hotelsofgreece.com/athens/electra-palace-hotel-pool.jpg'})
 
 type1=UserType.create({name:'Usuario Administrador'})
 type2=UserType.create({name:'Usuario Premium'})
