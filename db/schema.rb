d282d117764939f2c13f8cee0b0844aa7c0e9a47
barbicorro <barbicorro@hotmail.com>
2018-12-13 11:00:04 -0300

    Vista precios membresias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181212235534) do
+ActiveRecord::Schema.define(version: 20181213125716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20181212235534) do
   create_table "inscriptions", force: :cascade do |t|
     t.integer "week_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prices", force: :cascade do |t|
+    t.integer "premium"
+    t.integer "base"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
