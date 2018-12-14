041dc75560c7c2f186c8dae8130e7c46cf234134
Pauli17 <44780295+Pauli17@users.noreply.github.com>
2018-12-13 22:05:36 -0300

    Update registrations_controller.rb
    mm

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -12,7 +12,7 @@ class User::RegistrationsController < Devise::RegistrationsController
    super
    @user=User.new(params.required(:user).permit(:date_venc_month,:date_venc_year,:cod,:card_Number,
       :name,:image,:date_of_birth,:email,:lastname,:password,:password_confirmation,
-      :card_Marca)
+      :card_Marca))
     if @user.save
         redirect_to :back , notice: 'Usuario creado exitosamente'
     else
